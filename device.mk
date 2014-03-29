28112656db6d27b12b7c8ea32f37eba3d76bd15e
Tony Malagisi <mtb3000gt@gmail.com>
2014-03-29 14:14:06 -0400

    Change d2lte dpi

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=240
 else
 # These poor devices have smaller screens
 PRODUCT_AAPT_CONFIG := normal hdpi
